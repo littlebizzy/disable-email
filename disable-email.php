05767634ced47d724ec4fb9458b35cc9bcef5beb
Jesse Nickles <jesse@littlebizzy.com>
2024-10-25 14:23:42 +0700

    Update disable-email.php

--- disable-email.php
+++ disable-email.php
@@ -4,24 +4,25 @@ Plugin Name: Disable Email
 Plugin URI: https://www.littlebizzy.com/plugins/disable-email
 Description: Completely disables email sending
 Version: 1.0.0
+Requires PHP: 7.0
 Author: LittleBizzy
 Author URI: https://www.littlebizzy.com
 License: GPLv3
 License URI: http://www.gnu.org/licenses/gpl-3.0.txt
-GitHub Plugin URI: https://github.com/littlebizzy/disable-email
+GitHub Plugin URI: littlebizzy/disable-email
 Primary Branch: master
 */
 
-// Prevent direct access
+// prevent direct access
 if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-// Disable WordPress.org updates for this plugin
+// disable wordpress.org updates for this plugin
 add_filter( 'gu_override_dot_org', function( $overrides ) {
     $overrides[] = 'disable-email/disable-email.php';
     return $overrides;
-});
+}, 999 );
 
 // Disable all email sending in WordPress Core
 add_filter( 'wp_mail', function( $args ) {
